d5bbae06a28d6ab9206208d1ec693c4059a3e09c
mud66 <maddynielsen@outlook.com>
2025-04-20 15:21:48 +0100

    adding larger data files

--- Hybrid/Gatv2Conv.py
+++ Hybrid/Gatv2Conv.py
@@ -11,34 +11,29 @@ import matplotlib.pyplot as plt
 import torch.nn.functional as F
 from torch_geometric.nn import GATv2Conv
 import torch.nn as nn
-from torch.optim import AdamW
-from sklearn.metrics import mean_squared_error, mean_absolute_error
 
-import random
+
 class GATModel(nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels, num_heads, edge_feature_dim, dropout_rate=0.2):
         super(GATModel, self).__init__()
         self.dropout_rate = dropout_rate
 
-        # Define the GAT layers
         self.gat1 = GATv2Conv(in_channels, hidden_channels, heads=num_heads, edge_dim=edge_feature_dim)
         self.gat2 = GATv2Conv(hidden_channels * num_heads, hidden_channels, heads=num_heads, edge_dim=edge_feature_dim)
         self.gat3 = GATv2Conv(hidden_channels * num_heads, hidden_channels, heads=num_heads, edge_dim=edge_feature_dim)
         self.gat4 = GATv2Conv(hidden_channels * num_heads, hidden_channels, heads=num_heads, edge_dim=edge_feature_dim)
         self.gat5 = GATv2Conv(hidden_channels * num_heads, out_channels, heads=num_heads, concat=False, edge_dim=edge_feature_dim)
 
-        # LayerNorm layers (replacing BatchNorm)
         self.ln1 = nn.LayerNorm(hidden_channels * num_heads)
         self.ln2 = nn.LayerNorm(hidden_channels * num_heads)
         self.ln3 = nn.LayerNorm(hidden_channels * num_heads)
         self.ln4 = nn.LayerNorm(hidden_channels * num_heads)
 
-        # Dropout layer
         self.dropout = nn.Dropout(p=self.dropout_rate)
 
     def forward(self, x, edge_index, edge_attr):
         x = self.gat1(x, edge_index, edge_attr)
-        x = self.ln1(x)  # LayerNorm instead of BatchNorm
+        x = self.ln1(x) 
         x = F.elu(x)
         x = self.dropout(x)
 
